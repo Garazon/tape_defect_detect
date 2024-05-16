24d2297e5beed841a81d45ed68208a96e99c8e38
Garazon <1004604067@qq.com>
2024-05-16 13:43:37 +0800

    胶带缺陷检测

--- utils/box_utils.py
+++ utils/box_utils.py
@@ -1,16 +1,27 @@
-# 检测齐缝线是否平分胶带
+# 胶带缺陷检测相关工具
 import math
-
 import cv2
 
 
 # 定义两点之间距离计算的函数
 def cal_distance(point1, point2):
+    """
+    :param point1: 第一个点，包括x和y的值
+    :param point2: 第二个点，包括x和y的值
+    :return: 两点之间的距离，利用sqrt函数计算
+    """
+
     return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
 
 
 # 获取距离边界框四角距离最近的点作为实际四角的点
 def get_edge_points(pos_list, mask):
+    """
+    :param pos_list: 边界框坐标
+    :param mask: 掩码坐标
+    :return: edge_points: 掩码的四角坐标列表
+    """
+
     edge_points = []
     for point_box in pos_list:
         min_distance = float('inf')  # 初始设为无穷大
@@ -24,14 +35,14 @@ def get_edge_points(pos_list, mask):
     return edge_points
 
 
-# 判断齐缝线是否平分胶带
+# 判断齐缝线是否平分胶带,计算褶皱部分和胶带部分的面积
 def is_middle_line(results):
     """
-    :param   results: results  模型预测结果
+    :param   results： 模型预测结果
     :return: box_edge_points, 距离封箱面的边界框四角的点距离最近的点，形成新的列表
              tape_edge_points, 距离胶带的边界框四角的点距离最近的点，形成新的列表
              area_tape, 胶带面积
-             area_tape_defect 胶带缺陷面积
+             area_tape_defect, 胶带缺陷面积
     """
 
     try:
@@ -50,7 +61,7 @@ def is_middle_line(results):
                     if class_id == 0.0 or class_id == 3.0:  # 根据yaml文件更换需要判断的值，如果是盒子，不区分顶面和底面，获取掩码边界点
                         box_obj_pos = box.xyxy.squeeze().tolist()  # 将检测框坐标转化为列表
                         box_masks_pos = contour.tolist()
-                        print(box_obj_pos)
+                        # print(box_obj_pos)
                         box_obj_lists.append(box_obj_pos)  # 将检测框加入列表
                         # 把两部分箱面的掩码放进列表中
                         box_masks_lists += box_masks_pos
@@ -82,3 +93,55 @@ def is_middle_line(results):
     except Exception as e:
         print(f"An error occurred: {e}")
         return float('inf'), float('inf'), float('inf'), float('inf')
+
+# 判断箱喷码平分胶带
+def is_middle_code(pos_top_left_corner_y, area_mask):
+    """
+    :param pos_top_left_corner_y: 预测框左上角的纵坐标
+    :param area_mask: 掩码区域的面积
+    :return: top_ratio, 上部箱喷码差和比
+             bottom_ratio, 下部箱喷码差和比
+    """
+    try:
+        # 找到上部的箱喷码的纵坐标值
+        min_indexes = sorted(range(len(pos_top_left_corner_y)), key=lambda i: pos_top_left_corner_y[i])[:2]
+
+        # 根据索引找到对应的掩码面积
+        top_code_area = [area_mask[i] for i in min_indexes]
+        # 将下部的箱喷码的掩码面积加入到另一个数组中
+        bottom_code_area = [value for i, value in enumerate(area_mask) if i not in min_indexes]
+
+        # 计算上部箱喷码面积差和比
+        top_ratio = round(abs(top_code_area[0] - top_code_area[1]) / (top_code_area[0] + top_code_area[1]), 2)
+        # 计算下部箱喷码面积差和比
+        bottom_ratio = round(abs(bottom_code_area[0] - bottom_code_area[1]) / (bottom_code_area[0] + bottom_code_area[1]), 2)
+
+        return top_ratio, bottom_ratio
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return float('inf'), float('inf')
+
+# 处理每个类别的掩码和目标框
+def process_class(boxes, masks, class_id):
+    """
+    :param boxes: 预测框
+    :param masks: 掩码
+    :param class_id: 类别id
+    :return: edge_points, 指定类别掩码角落的点
+    """
+    try:
+        for box, mask in zip(boxes, masks):
+            if box.cls == class_id:  # 如果是指定类别，获取掩码边界点
+                box_obj_pos = box.xyxy.squeeze().tolist()  # 将检测框坐标转化为列表
+                box_masks_pos = mask.xy[0].tolist()
+                # 存储边界框位置信息，以四个点对形式存储
+                box_pos_list = [[box_obj_pos[0], box_obj_pos[1]],
+                                [box_obj_pos[2], box_obj_pos[1]],
+                                [box_obj_pos[0], box_obj_pos[3]],
+                                [box_obj_pos[2], box_obj_pos[3]]]
+                # 获取指定类别的掩码角落的点
+                edge_points = get_edge_points(box_pos_list, box_masks_pos)
+        return edge_points
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return float('inf')
