ef04a785b4774da1e832272703b9e62adc196f2d
Garazon <1004604067@qq.com>
2024-05-21 11:38:44 +0800

    正面文本区域检测更新

--- front_text_area.py
+++ front_text_area.py
@@ -21,9 +21,9 @@ def text_area(results):
                     if cls_id == 2.0:  # 判断是奖项区域
                         reward = box.xyxy.squeeze().tolist()  # 将检测框坐标转化为列表
 
-        produce_date = np.array(produce_date)
-        introduce = np.array(introduce)
-        reward = np.array(reward)
+        # produce_date = np.array(produce_date)
+        # introduce = np.array(introduce)
+        # reward = np.array(reward)
         return produce_date, introduce, reward
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -34,6 +34,16 @@ if __name__ == '__main__':
     # 加载模型权重
     model = YOLO('models/text_area-seg.onnx')  # 从YAML构建并转移权重
     device = "cuda:0"
-    img = r'test_img/test_text.jpg'
+    img = r'test_img/test_wu.jpg'
     results = model(img, save=False, show_boxes=False, device=device)  # 返回一个结果对象列表
     produce_date, introduce, reward = text_area(results)
+    print("箱喷码生产日期：", produce_date)
+    print("规格：", introduce)
+    print("奖项：", reward)
+
+    information = {
+        "produce_date": produce_date,
+        "introduce": introduce,
+        "text_area": reward
+    }
+    print(information)
